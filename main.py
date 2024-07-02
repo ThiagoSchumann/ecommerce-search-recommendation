f730f1c5ce2c043ee0ffc047df323e0678d1ec68
Adrian Grosch <adrian.grosch@singular.inf.br>
2024-07-01 22:58:15 -0300

    feat: Integrate RabbitMQ with Flask application
    - Update docker-compose.yml to pass RabbitMQ credentials as environment variables to Flask service.
    - Modify main.py to add a route for RabbitMQ connection and message publishing.
    - Update requirements.txt to include pika library for RabbitMQ interaction.

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 from flask import Flask
+from pika.credentials import PlainCredentials
+import os
 
 app = Flask(__name__)
+import pika
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/rabbitmq")
+def rabbitmq_connection():
+    try:
+        username=os.environ['RABBITMQ_DEFAULT_USER']
+        password=os.environ['RABBITMQ_DEFAULT_PASS']
+        credentials = PlainCredentials(username=username,password=password)
+        connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq',credentials=credentials))
+        channel = connection.channel()
+        channel.queue_declare(queue='hello')
+        channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body='Hello World!')
+        return "<p>Rabbitmq is connected</p>"
+    except Exception as e:
+        return str(e)
