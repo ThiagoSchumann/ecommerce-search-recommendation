a004e69470ad3356d31e8d98ea1cd59c93a43e22
Adrian Grosch <adriangrosch15@gmail.com>
2024-07-09 20:42:09 -0300

    Refactor and modularize infrastructure setup and main application
    - Split Docker Compose configuration into separate files: one for infrastructure (docker-compose-infra.yml) and one for the application (docker-compose.yml).
    - Modularize application initialization by creating an Application class to handle setup and running of server components.
    - Update main.py to use the new Application class for starting the application.
    - Move logging setup to logger_adapter.py and create a LoggingPort to manage logging operations.
    - Refactor HttpServer to initialize the Flask app and setup API documentation within the class.
    - Ensure that all components are properly connected and initialized via dependency injection.
    - Added configurations and setup to support testing environments.
    
    Preparing the project setup to be fully available for tests.

--- main.py
+++ main.py
@@ -1,37 +1,12 @@
-import os
-import logging
-from src.infra.message_broker.message_broker_port import message_broker
-from src.infra.orm.orm_port import orm
-from src.infra.http_server.http_server import HttpServer
-from src.infra.dependency_injection import configure_dependencies
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+from src.infra.application.application import Application
+from src.infra.logging.logging_port import logger
+
 
 def main():
     try:
-        logger.info("Connecting to message broker")
-        message_broker.connect(
-            username=os.environ['RABBITMQ_DEFAULT_USER'],
-            password=os.environ['RABBITMQ_DEFAULT_PASS']
-        )
-
-        logger.info("Connecting to the database")
-        orm.connect_to_database(
-            user=os.environ['DB_USER'],
-            password=os.environ['DB_PASSWORD'],
-            host=os.environ['DB_HOST'],
-            port=os.environ['DB_PORT'],
-            database=os.environ['DB_NAME'],
-        )
-        orm.create_tables()
-
-        logger.info("Configuring API documentation adapter")
-        api_docs_adapter = configure_dependencies()
-
-        logger.info("Starting HTTP server")
-        http_server = HttpServer(api_docs_adapter)
-        http_server.run()
+        application = Application()
+        application.run_server()
     except Exception as e:
         logger.error(f"Error during initialization: {e}", exc_info=True)
         raise
--- src/infra/http_server/http_server.py
+++ src/infra/http_server/http_server.py
@@ -5,14 +5,10 @@ from src.infra.api_docs.api_docs_port import APIDocsPort
 class HttpServer:
     def __init__(self, api_docs_port: APIDocsPort):
         self.api_docs_port = api_docs_port
-
+        self.app = Flask(__name__)
+        self.app.register_blueprint(product_blueprint)
+        self.app.config['JSON_SORT_KEYS'] = False
+        self.api_docs_port.setup(self.app)
+        
     def run(self):
-        app = Flask(__name__)
-        app.register_blueprint(product_blueprint)
-        app.config['JSON_SORT_KEYS'] = False
-
-        # Configuração da documentação da API via porta
-        self.api_docs_port.setup(app)
-
-        app.run(host='0.0.0.0', debug=True)
-
+        self.app.run(host='0.0.0.0', debug=True)
