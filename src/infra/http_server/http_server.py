6651b80062254824529234033f60139db69b5a23
Adrian Grosch <adriangrosch15@gmail.com>
2024-07-10 20:03:07 -0300

    Add category repository and routes
    - Created CategoryRepository with methods for CRUD operations.
    - Added category_routes.py to handle HTTP requests for category operations.
    - Modified ProductRepository to link products with categories.
    - Updated product_routes.py to include category_id when creating products.
    - Refactored http_server.py to register category routes.
    - Updated __init__.py in routes to import and expose new routes.

--- src/infra/http_server/http_server.py
+++ src/infra/http_server/http_server.py
@@ -1,12 +1,16 @@
 from flask import Flask
-from src.domain.routes.product_routes import product_blueprint
 from src.infra.api_docs.api_docs_port import APIDocsPort
+from src.domain.routes import *
 
 class HttpServer:
+    def register_blueprints(self):
+        self.app.register_blueprint(product_blueprint)
+        self.app.register_blueprint(category_blueprint)
+        
     def __init__(self, api_docs_port: APIDocsPort):
         self.api_docs_port = api_docs_port
         self.app = Flask(__name__)
-        self.app.register_blueprint(product_blueprint)
+        self.register_blueprints()
         self.app.config['JSON_SORT_KEYS'] = False
         self.api_docs_port.setup(self.app)
         
